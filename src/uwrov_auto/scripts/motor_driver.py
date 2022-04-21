7c7b282ad8ad88edeb4feaa454f6a80b1a9e950b
Lauren Krieger <lkrieger104@gmail.com>
2022-04-20 18:53:12 -0700

    patch 1

--- src/uwrov_auto/scripts/motor_driver.py
+++ src/uwrov_auto/scripts/motor_driver.py
@@ -1,13 +1,17 @@
+from asyncio.windows_events import NULL
 import pigpio
 import rospy
+import threading
 from std_msgs.msg import Int16MultiArray
 from std_msgs.msg import MultiArrayDimension
 from std_msgs.msg import MultiArrayLayout
+from std_msgs.msg import String
 
 listen_topic = '/nautilus/motors/pwm'
 
 dims = [MultiArrayDimension('data', 6, 16)]
 layout = MultiArrayLayout(dim=dims, data_offset=0)
+lock = threading.Lock()
 
 pi = pigpio.pi()
 # thruster order: ['forward_left', 'forward_right', 'forward_top', 'sideways_top', 'up_left', 'up_right']
@@ -23,8 +27,21 @@ thruster_pins = {
 pins = list(thruster_pins.values())
 
 def pwm_apply_callback(msg):
-    for pin, pwm in zip(pins, msg.data):
-        pi.set_servo_pulsewidth(pin, pwm)
+    if not lock.locked():
+        for pin, pwm in zip(pins, msg.data):
+            pi.set_servo_pulsewidth(pin, pwm)
+
+def arm_motors():
+    if not lock.locked():
+        t = threading.Thread(target=arm_thread)
+        t.start()
+
+def arm_thread():
+    lock.acquire()
+    for key, val in thruster_pins:
+        pi.set_servo_pulsewidth(key, 0)
+    lock.release()
+    # kill thread?
 
 
 def main():
@@ -32,6 +49,7 @@ def main():
 
     rospy.init_node('motor_driver')
     rospy.Subscriber(listen_topic, Int16MultiArray, pwm_apply_callback)
+    # rospy.Subscriber(arm_topic, String, pwm_apply_callback)
     rospy.on_shutdown(shutdown_fn)
     rospy.spin()
 
